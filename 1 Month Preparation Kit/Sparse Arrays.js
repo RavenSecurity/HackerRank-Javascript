c554c8401428909e7d22d0287647625526bd769b
Rayan <ravenSec93@gmail.com>
2022-10-26 00:25:22 +0200

    26 October

--- 1 Month Preparation Kit/Sparse Arrays.js
+++ 1 Month Preparation Kit/Sparse Arrays.js
@@ -1,17 +1,79 @@
-let strings = ["aba", "baba", "vaba", "vxzxb", 3, "aba", "xzxb", "ab"];
-let queries = ["aba", "vaba", 3];
+'use strict';
 
-// let result = []
-let result = strings.filter((i) => queries.includes(i));
+const fs = require('fs');
 
-const count = {};
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
 
-for (const element of result) {
-  if (count[element]) {
-    count[element] += 1;
-  } else {
-    count[element] = 1;
-  }
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
 
-console.log(count)
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(strings, queries) {
+    // Write your code here
+const findFrequency = (arr1 = [], arr2 = []) => {
+   const res = [];
+   let count = 0;
+   for (let i = 0; i < arr2.length; i++){
+      for (let j = 0; j < arr1.length; j++){
+         if (arr2[i] === arr1[j]){
+            count++;
+         }
+      }
+      res.push(count);
+      count = 0;
+   }
+   return res;
+};
+return findFrequency(strings, queries)
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const stringsCount = parseInt(readLine().trim(), 10);
+
+    let strings = [];
+
+    for (let i = 0; i < stringsCount; i++) {
+        const stringsItem = readLine();
+        strings.push(stringsItem);
+    }
+
+    const queriesCount = parseInt(readLine().trim(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    const res = matchingStrings(strings, queries);
+
+    ws.write(res.join('\n') + '\n');
+
+    ws.end();
+}
