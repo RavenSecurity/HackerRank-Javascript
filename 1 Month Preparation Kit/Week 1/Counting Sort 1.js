d92b620a40b48681ba35fc0306e791fb444a9199
Rayan <ravenSec93@gmail.com>
2022-10-31 20:01:29 +0100

    31 October

--- 1 Month Preparation Kit/Week 1/Counting Sort 1.js
+++ 1 Month Preparation Kit/Week 1/Counting Sort 1.js
@@ -1,10 +1,49 @@
-arr = [93, 93, 69, 69, 69, 7]
-const count = []
+'use strict';
 
-for(let i = 0; i < arr.length; i++) {
-    (count.push(arr.filter(x => x==arr[i]).length-1))
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
 
-console.log(count)
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
 
-// WTF I don't even understand the problem
+function countingSort(arr) {
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = countingSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
